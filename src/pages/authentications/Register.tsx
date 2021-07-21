a39325831d1f3cc758ac5165c14a432e0fa45134
DanBertrand <dan.bertrand.hs@gmail.com>
2021-07-21 09:47:58 +0200

    Custom form with formik / Add validation scheme

--- src/pages/authentications/Register.tsx
+++ src/pages/authentications/Register.tsx
@@ -1,46 +1,71 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchToRegister } from '../../stores/authentication/authMiddleware';
+import * as Yup from 'yup';
+import { Formik, Field, Form } from 'formik';
+
+type Values = {
+    email: string;
+    password: string;
+};
 
 const Register: React.FC = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
     const dispatch = useDispatch();
 
-    const register = async () => {
+    const register = async (values: Values) => {
         const data = {
-            user: {
-                email: email,
-                password: password,
-            },
+            user: values,
         };
-        console.log('REGISTERING SOON:', data);
         dispatch(fetchToRegister(data));
     };
 
+    const RegisterSchema = Yup.object().shape({
+        email: Yup.string().email('Invalid email').required('Required'),
+        password: Yup.string().min(3, 'Too Short!').max(50, 'Too Long!').required('Required'),
+    });
+
     return (
-        <div className="Register">
+        <div>
             <h1>This is register</h1>
-            <form>
-                <input
-                    type="text"
-                    placeholder="email"
-                    autoComplete="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="password"
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />{' '}
-                <br />
-                <input type="button" onClick={register} value="Valider l'inscription" />
-            </form>
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: '',
+                }}
+                validationSchema={RegisterSchema}
+                onSubmit={(values: Values) => {
+                    console.log(values);
+                    register(values);
+                }}
+            >
+                {({ errors, touched }) => (
+                    <Form>
+                        <label htmlFor="email">Email</label>
+                        <Field name="email" type="email" autoComplete="email" />
+                        {errors.email && touched.email ? <div>{errors.email}</div> : null}
+                        <label htmlFor="password">Password</label>
+                        <Field
+                            type={showPassword ? 'text' : 'password'}
+                            name="password"
+                            autoComplete="password"
+                            className="form-control rounded-0"
+                        />
+                        {errors.password && touched.password ? <div>{errors.password}</div> : null}
+                        <label>
+                            <Field
+                                onChange={() => setShowPassword(!showPassword)}
+                                type="checkbox"
+                                checked={showPassword}
+                                name="toggle"
+                            />
+                            Show password
+                        </label>
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
 };
