6706160ec365f813d48269740496405ffa481315
DanBertrand <dan.bertrand.hs@gmail.com>
2021-07-21 13:13:21 +0200

    Log Infos on navbar + buttons login register logout

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
 import styled from 'styled-components';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { StoreStateType } from '../../stores/index';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import { fetchToLogout } from '../../stores/authentication/authMiddleware';
 
 const Navbar: React.FC = () => {
-    const user = useSelector((state: StoreStateType) => state.auth);
-    return <Container>{user.isLogged ? <div>You logged !</div> : <button>Login</button>}</Container>;
+    const { isLogged, token, email } = useSelector((state: StoreStateType) => state.auth);
+    const dispatch = useDispatch();
+    console.log('USER NAV BAR', isLogged);
+
+    const handleLogout = () => {
+        dispatch(fetchToLogout(token));
+    };
+
+    return (
+        <Container>
+            {isLogged ? (
+                <>
+                    <div>You logged !</div>
+                    <h1>Logged as: {email}</h1>
+                    <button type="button" onClick={handleLogout}>
+                        Logout
+                    </button>
+                </>
+            ) : (
+                <>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </>
+            )}
+        </Container>
+    );
 };
 
 export default Navbar;
@@ -16,4 +42,5 @@ const Container = styled.div`
     background-color: dodgerblue;
     display: flex;
     align-items: center;
+    justify-content: space-evenly;
 `;
