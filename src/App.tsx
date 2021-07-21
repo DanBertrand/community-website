b91ed6ca9c12d84d476356d79752c792d099ff68
DanBertrand <dan.bertrand.hs@gmail.com>
2021-07-21 09:48:51 +0200

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Navbar from './components/layout/Navbar';
 // import styled from 'styled-components';
 // import mainImage from './assets/images/main-image.jpg';
-// import Login from './pages/authentications/Login';
 import Register from './pages/authentications/Register';
-
-interface StoreState {
-    auth: {
-        currentUser: string;
-        token: string;
-    };
-}
+import Home from './pages/Home';
+import { StoreStateType } from './stores/index';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/authentications/Login';
 
 const App: React.FC = () => {
-    const state = useSelector((state: StoreState) => state.auth);
-
+    const state = useSelector((state: StoreStateType) => state.auth);
     console.log('Store', state);
 
-    return <Register />;
+    return (
+        <Router>
+            <Navbar />
+            <Home />
+            <Switch>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+            </Switch>
+        </Router>
+    );
 };
 
 // const Container = styled.div`
