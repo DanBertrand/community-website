8d2512a791285db5a6267efa35d92ca76e453390
DanBertrand <dan.bertrand.hs@gmail.com>
2021-09-29 18:42:57 +0200

    Upgrade Email confirmation conponent

--- src/pages/authentications/EmailConfirmation.tsx
+++ src/pages/authentications/EmailConfirmation.tsx
@@ -2,49 +2,37 @@ import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import Loading from '../../components/Loading';
 import Modal from '../../components/Modal';
+import useFetch from '../../hooks/useFetch';
 
 const EmailConfirmation = (): JSX.Element => {
-    const [message, setMessage] = React.useState('');
-    const [error, setError] = React.useState('');
-    const [didFetch, setDidFetch] = React.useState(false);
     const history = useHistory();
     const location = useLocation();
+    const [counter, setCounter] = React.useState(4);
+    const { state, confirmToken, resendConfirmationEmail } = useFetch();
+    const { isLoading, error, message } = state;
     const confirmationToken = location.search.split('=').pop();
 
-    const API_VERSION_URL = process.env.REACT_APP_API_VERSION_URL;
-    const HOST_URL = process.env.REACT_APP_HOST_URL;
-    const API_URL = `${HOST_URL}${API_VERSION_URL}`;
+    const callback = () => {
+        setTimeout(() => {
+            history.push('/login');
+        }, 4000);
+    };
 
     const resendEmail = async () => {
-        console.log('Resend Email');
-        const response = await fetch(`${API_URL}/user/confirmation/request_new_link/${confirmationToken}`, {
-            method: 'GET',
-        });
-        console.log(response);
-        const data = await response.json();
-        console.log('response USER', data.data);
+        setCounter(4);
+        resendConfirmationEmail(`/user/confirmation/request_new_link/${confirmationToken}`, callback);
     };
 
     React.useEffect(() => {
-        const confirmToken = async () => {
-            const resp = await fetch(`${HOST_URL}/confirmation?confirmation_token=${confirmationToken}`);
-            console.log(resp);
-            if (resp.ok) {
-                setTimeout(() => {
-                    history.push('/login');
-                }, 3000);
-            }
-            const data = await resp.json();
-            console.log('response token', data);
-            const { message, error } = data;
-            message && setMessage(message);
-            error && setError(error);
-            setDidFetch(true);
-        };
-        confirmToken();
-    }, [confirmationToken]);
+        const timer = setInterval(() => setCounter((counter) => counter - 1), 1000);
+        return () => clearInterval(timer);
+    }, [counter, message]);
 
-    if (!didFetch) {
+    React.useEffect(() => {
+        confirmToken(`/confirmation?confirmation_token=${confirmationToken}`, callback);
+    }, []);
+
+    if (isLoading) {
         return <Loading />;
     } else {
         return (
@@ -54,10 +42,13 @@ const EmailConfirmation = (): JSX.Element => {
                     {error && error}
                 </h2>
                 <p> You can request a new confirmation link</p>
-
-                <button type="button" onClick={resendEmail}>
-                    Resend Email
-                </button>
+                {message ? (
+                    <span>Redirect to login in {counter} </span>
+                ) : (
+                    <button type="button" onClick={resendEmail}>
+                        Resend Email
+                    </button>
+                )}
             </Modal>
         );
     }
