19095898adf1f624ef108ac862251cc0c3d97755
DanBertrand <dan.bertrand.hs@gmail.com>
2021-09-30 15:26:56 +0200

    Update export with new hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,5 @@ export { useActions } from './useActions';
 export { useTypedSelector } from './useTypedSelector';
 export { useFetch } from './useFetch';
 export { useDebounce } from './useDebounce';
+export { useClientInfos } from './useClientInfos';
+export { useGeoLocation } from './useGeoLocation';
