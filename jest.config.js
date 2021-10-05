dc313a3d53f99f2b0a54326f46c3956cd815e28d
DanBertrand <dan.bertrand.hs@gmail.com>
2021-10-05 21:18:44 +0200

    Create jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     preset: 'ts-jest',
+    resetMocks: true,
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/__test__/config/importJestDOM.ts'],
     moduleDirectories: ['node_modules', 'src'],
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        '^.+\\.(js|jsx)$': 'ts-jest',
+    },
 };
